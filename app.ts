a67089c31b60370a7aeab9c0693b1f0c155e4224
dusagong <yoonseungjae123@gmail.com>
2025-01-31 15:05:32 +0900

    set init template

---
+++ app.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import type { Request, Response, NextFunction } from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import authRouter from "./routes/auth_route.js";
+import { prisma } from "./prisma.js";  // prisma 인스턴스 import
+
+dotenv.config();
+export const app = express();
+
+prisma.$connect().then(()=> {
+   console.log("Connected to DB");
+   if(process.env.MODE === "test") {
+       app.listen(process.env.PORT, () => {
+           console.log(`Server is running on port ${process.env.PORT}`);
+       })
+   } else {
+       app.listen(process.env.PORT, () => {
+           console.log(`Server is running on port ${process.env.PORT}`);
+       })
+   }
+});
+
+app.set("port", process.env.PORT || 3030);
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
+
+type CustomError = Error & { status?: number };
+
+app.use((err: CustomError, req: Request, res: Response, next: NextFunction) => {
+   console.log(err);
+
+   const errorResponse = {
+       message: err.message || "Internal Server Error",
+       status: err.status || 500,
+   }
+
+   res.status(errorResponse.status).json(errorResponse);
+});
+
+export default app;
---
+++ readme.md
@@ -0,0 +1,6 @@
+ 필요한 라이브러리 설치
+서버 구동: Express
+DB 연동: Prisma / @prisma/client
+비밀번호 해시: bcrypt
+JWT 토큰: jsonwebtoken
+
